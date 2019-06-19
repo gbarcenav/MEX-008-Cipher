82b72457045c85077068876dfa76379b4ee89251
Gabriela Barcena <g.barcena.v@gmail.com>
2019-06-19 01:27:01 -0500

    Agrgué ejemplo cifrar a Cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,15 @@
 window.cipher = {
-  // ... 
+  // Función cifrar
+const encode = (str, offset) => {
+  str = str.toUpperCase();
+  let encryptedStr = "";
+  let encryptedChar = "";
+  for (let i = 0; i < str.length; i++) {
+    let charASCII = str.charCodeAt(i);
+    encryptedChar = ((charASCII - 65 + offset) % 26) + 65;
+    encryptedStr = encryptedStr + String.fromCharCode(encryptedChar);
+  }
+  return encryptedStr;
+};
+encode("Adios", 2);
 };
